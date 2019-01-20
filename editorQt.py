8ddb98b84e6bd0c251a6d19f2fddd68aeae5c99d
Nicolas <duchonic@bluewin.ch>
2019-01-20 09:21:55 +0800

    issues closed

--- editorQt.py
+++ editorQt.py
@@ -4,7 +4,7 @@
 Created on Sun Jan  6 19:20:58 2019
 
 dOS (duchoud Operating System)
-manage weekly hours with lego technic bricks
+manage weekly hours with lego technic bricks (prototype)
 
 @author: duchonic
 
@@ -19,9 +19,11 @@ from PyQt5.QtWidgets import QProgressBar, QHBoxLayout, QVBoxLayout
 class App(QWidget):
 
     buttonList = list()
-    thTest = 0
+    _thUpdate = 0
     currentTime = 0
     mPlan = dict()
+
+    _WORKSTRINGS = ("nothing", "paid work", "learn", "sleep")
 
     class Button():
 
@@ -32,7 +34,6 @@ class App(QWidget):
         layout = 0
         actualColor = 0
 
-
         COLORS = ("grey", "red", "yellow", "black")
 
         def __init__(self, day, hour, form, color, hboxTable):
@@ -41,7 +42,10 @@ class App(QWidget):
             self.hour = hour
             self.form = form
             self.btn = QPushButton( str(hour), form)
-            self.actualColor = color
+            if color < len(self.COLORS):
+                self.actualColor = color
+            else:
+                self.actualColor = 0
             self.btn.setStyleSheet("color: pink; background-color: " + self.COLORS[self.actualColor])
             self.btn.setToolTip(str(day*24 + hour))
             hboxTable.addWidget(self.btn)
@@ -69,7 +73,6 @@ class App(QWidget):
 
     def initUI(self):
 
-
         try:
             with open('data.json','r') as readData:
                 self.mPlan = json.load(readData)
@@ -82,10 +85,10 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        buttonQuit = QPushButton('quit', self)
-        buttonStart = QPushButton('start', self)
-        buttonSave = QPushButton('save', self)
-        buttonLoad = QPushButton('load', self)
+        # self.buttonQuit = QPushButton('quit', self)
+        # self.buttonStart = QPushButton('start', self)
+        # self.buttonSave = QPushButton('save', self)
+        # self.buttonLoad = QPushButton('load', self)
 
         self.work = QLabel(self)
         self.work.setText('')
@@ -97,106 +100,126 @@ class App(QWidget):
         self.countdownTime = QLabel(self)
         self.countdownTime.setText('')
 
-        buttonQuit.clicked.connect(self.threadStop)
-        buttonStart.clicked.connect(self.threadUpdate)
-        buttonSave.clicked.connect(self.savePlan)
-        buttonLoad.clicked.connect(self.loadPlan)
+        # self.buttonQuit.clicked.connect(self.threadStop)
+        # self.buttonStart.clicked.connect(self.threadUpdate)
+        # self.buttonSave.clicked.connect(self.savePlan)
+        # self.buttonLoad.clicked.connect(self.loadPlan)
 
         self.progress = QProgressBar(self)
         self.progress.setGeometry(10,250,300,50)
         self.progress.setMaximum(100)
 
 
-        vbox = QVBoxLayout()
-
-        hboxTop = QHBoxLayout()
-
-        hboxTop.addWidget(buttonQuit)
-        hboxTop.addWidget(buttonStart)
-        hboxTop.addWidget(buttonSave)
-        hboxTop.addWidget(buttonLoad)
-        vbox.addLayout(hboxTop)
+        _vbox = QVBoxLayout()
+        # _hboxTop = QHBoxLayout()
+        #
+        # _hboxTop.addWidget(self.buttonQuit)
+        # _hboxTop.addWidget(self.buttonStart)
+        # _hboxTop.addWidget(self.buttonSave)
+        # _hboxTop.addWidget(self.buttonLoad)
+        # _vbox.addLayout(_hboxTop)
 
         for day in range(7):
-            hboxTable = QHBoxLayout()
+            _hboxTable = QHBoxLayout()
             for hour in range(24):
                 try:
                     mTime = datetime.datetime.utcnow().isocalendar()
                     color = self.mPlan[str(mTime[0])][str(mTime[1])].get(str(day*24+hour))
                 except:
                     color = 0
-                self.buttonList.append( self.Button(day, hour, self, color, hboxTable) )
-            vbox.addLayout(hboxTable)
+                self.buttonList.append( self.Button(day, hour, self, color, _hboxTable) )
+            _vbox.addLayout(_hboxTable)
 
-        vbox.addWidget(self.work)
-        vbox.addWidget(self.currentTime)
-        vbox.addWidget(self.countdownTime)
-        vbox.addWidget(self.progress)
+        _vbox.addWidget(self.work)
+        _vbox.addWidget(self.currentTime)
+        _vbox.addWidget(self.countdownTime)
+        _vbox.addWidget(self.progress)
 
-        self.setLayout(vbox)
+        self.setLayout(_vbox)
 
-        self.thTest = threading.Timer(1, self.threadUpdate)
-        self.thTest.start()
+        self._thUpdate = threading.Timer(1, self.threadUpdate)
+        self._thUpdate.start()
 
         self.show()
 
-    def showTime(self):
-        now = time.localtime( )
-        actItem = now.tm_wday*24  + now.tm_hour
-        workStr = str('act work: ') + str(self.buttonList[actItem].getColor())
+    def closeEvent(self, event):
+        print('closed with red cross')
+        self._thUpdate.cancel()
+        self.savePlan()
 
-        #check if more than one block is left
-        #todo check block in the past and add it to showTimeBlock
-        showTimeBlock = 0
-        showTimeActItem = actItem
+    def calcItemPos(self, _day, _hour):
+        return (_day*24)+_hour
+
+    def showTime(self):
+
+        _now = time.localtime()
+        _actItem = self.calcItemPos(_now.tm_wday, _now.tm_hour)
+        _showTimeBlockLeft = 0
+        _showTimeBlockPast = 0
+
+        _showTimeActItem = _actItem
         try:
-            while self.buttonList[showTimeActItem+1].getColor() == self.buttonList[showTimeActItem].getColor():
-                showTimeBlock += 1
-                showTimeActItem += 1
+            while self.buttonList[_showTimeActItem-1].getColor() \
+                    == self.buttonList[_showTimeActItem].getColor():
+                _showTimeBlockPast += 1
+                _showTimeActItem -= 1
         except:
             pass
-        showTimeTotal = (showTimeBlock+1) * 60
-        showTimeLeft  = ((showTimeBlock*60) + 59-now.tm_min)
-        currentTimeStr = str(now.tm_hour) + ':' + str(now.tm_min) + ':' + str(now.tm_sec)
-        countDownStr = str(showTimeLeft) + ':' + str(59-now.tm_sec)
 
-        self.progress.setValue( 100 -  (showTimeLeft / showTimeTotal)*100 )
+        _showTimeActItem = _actItem
+        try:
+            while self.buttonList[_showTimeActItem+1].getColor() \
+                    == self.buttonList[_showTimeActItem].getColor():
+                _showTimeBlockLeft += 1
+                _showTimeActItem += 1
+        except:
+            pass
 
-        self.currentTime.setText('current time: ' + currentTimeStr)
-        self.countdownTime.setText('time left: ' + countDownStr)
-        self.work.setText(workStr)
+        _showTimeTotal = (_showTimeBlockLeft+1 + _showTimeBlockPast)*60
+        _showTimeLeft  = ((_showTimeBlockLeft*60) + 59-_now.tm_min)
+
+        _workStr = str('dOS activity: ') + self._WORKSTRINGS[self.buttonList[_actItem].getColor()]
+        _currentTimeStr = str(_now.tm_hour).zfill(2) + ':' \
+                            + str(_now.tm_min).zfill(2) + ':' + str(_now.tm_sec).zfill(2)
+        _countDownStr = str(_showTimeLeft).zfill(2) + ':' + str(59-_now.tm_sec).zfill(2)
+
+        self.progress.setValue(100-(_showTimeLeft/_showTimeTotal)*100)
+        self.work.setText(_workStr)
+        self.currentTime.setText('current time: ' + _currentTimeStr)
+        self.countdownTime.setText('time left: ' + _countDownStr)
         self.show()
 
     def threadUpdate(self):
-        self.thTest = threading.Timer(1, self.threadUpdate)
-        self.thTest.start()
+        self._thUpdate = threading.Timer(1, self.threadUpdate)
+        self._thUpdate.start()
         self.showTime()
 
-    def threadStop(self):
-        print('kill update')
-        self.thTest.cancel()
-        self.savePlan()
-        self.show()
+    # def threadStop(self):
+    #     print('kill update')
+    #     self.thTest.cancel()
+    #     self.savePlan()
+    #     self.show()
 
     def getPlan(self):
-        plan = dict()
+        _plan = dict()
         for day in range(7):
             for hour in range(24):
-                mItem = day*24+hour
-                plan.update({ mItem : self.buttonList[mItem].getColor() })
-        return plan
+                mItem = self.calcItemPos(day,hour)
+                _plan.update({ mItem : self.buttonList[mItem].getColor() })
+        return _plan
 
     def savePlan(self):
-        mTime = datetime.datetime.utcnow().isocalendar()
-        self.mPlan[str(mTime[0])][str(mTime[1])] = self.getPlan()
+        _Time = datetime.datetime.utcnow().isocalendar()
+        self.mPlan[str(_Time[0])][str(_Time[1])] = self.getPlan()
         with open('data.json', 'w') as wrData:
+            print('saved plan')
             json.dump(self.mPlan, wrData, indent=2, ensure_ascii=False)
-
-    def loadPlan(self):
-        with open('data.json') as rdData:
-            storedPlan = json.load(rdData)
-        print(json.dumps(storedPlan, indent=2))
-        print('load plan')
+    #
+    # def loadPlan(self):
+    #     with open('data.json') as rdData:
+    #         storedPlan = json.load(rdData)
+    #     print(json.dumps(storedPlan, indent=2))
+    #     print('load plan')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
